6429e9f1442ed9456a78e8d7cf0bfcfc736c3184
JDev129 <jchristman129@gmail.com>
2019-11-12 22:16:04 -0500

    Update SudokuKeyPad.js

--- SudokuKeyPad.js
+++ SudokuKeyPad.js
@@ -13,11 +13,14 @@ class SudokuKeyPad extends React.Component {
       disable7: true,
       disable8: true,
       disable9: true,
+      disableclear: true,
       setNumber: props.setNumber,
       watchForClicks: props.watchForClicks,
       row: props.row,
       col: props.col,
       table: props.table,
+      //disableAll: props.disableAll,
+      currentActiveMoveName: props.currentActiveMoveName,
       possibleCandidates: props.possibleCandidates,
       setDisable: this.setDisable,
       buildDisableTable: this.buildDisableTable,
@@ -25,7 +28,48 @@ class SudokuKeyPad extends React.Component {
       currentActive: this.currentActive,
       resetHighlight: this.resetHighlight,
       addTheseRows: this.addTheseRows,
-      allowAnyEntry: this.allowAnyEntry
+      allowAnyEntry: this.allowAnyEntry,
+      sudokuTableName: props.sudokuTableName,
+      transparent: false
+    };
+  }
+  findClickPosition(e) {
+    var posx = 0;
+    var posy = 0;
+
+    if (e.pageX || e.pageY) {
+      var windowWidth = window.innerWidth;
+      //var windowHeight = window.innerHeight;
+
+      if (windowWidth > 991) {
+        //had to customize the result with a polynomial equation to account for screen resizing
+        posx =
+          e.pageX -
+          (-0.0000037 * windowWidth * windowWidth +
+            0.5093155 * windowWidth -
+            465.4196263);
+      } else if (windowWidth < 992 && windowWidth > 767) {
+        posx =
+          e.pageX -
+          (0.0004 * windowWidth * windowWidth - 0.2205 * windowWidth - 28);
+      } else if (windowWidth < 768) {
+        posx = e.pageX - 23;
+      }
+      posy = e.pageY - 79;
+    } else if (e.clientX && e.clientY) {
+      posx =
+        e.clientX +
+        document.body.scrollLeft +
+        document.documentElement.scrollLeft;
+      posy =
+        e.clientY +
+        document.body.scrollTop +
+        document.documentElement.scrollTop;
+    }
+
+    return {
+      x: posx,
+      y: posy
     };
   }
   currentActive() {
@@ -37,13 +81,27 @@ class SudokuKeyPad extends React.Component {
     }
     return theResult;
   }
-  buildDisableTable(table, row, col, writeIn, allowAnyEntry) {
+  buildDisableTable(
+    table,
+    row,
+    col,
+    writeIn,
+    allowAnyEntry,
+    theCurrentActiveMoveName
+  ) {
     let theResult = {};
-    if (allowAnyEntry) {
+    if (theCurrentActiveMoveName === "complete") {
+      theResult["disableclear"] = true;
+      for (let j = 1; j < 10; j++) {
+        theResult["disable" + j.toString()] = true;
+      }
+    } else if (allowAnyEntry) {
+      theResult["disableclear"] = false;
       for (let j = 1; j < 10; j++) {
         theResult["disable" + j.toString()] = false;
       }
     } else {
+      theResult["disableclear"] = false;
       let currentAvNumber = this.possibleCandidates(table, row, col)
         .numberArray;
       for (let j = 1; j < 10; j++) {
@@ -65,7 +123,8 @@ class SudokuKeyPad extends React.Component {
   static getDerivedStateFromProps(props, state) {
     return props.row !== state.row &&
       props.col !== state.col &&
-      props.table !== state.table
+      props.table !== state.table &&
+      props.currentActiveMoveName !== state.currentActiveMoveName
       ? state.buildDisableTable(
           props.table,
           props.row,
@@ -75,7 +134,64 @@ class SudokuKeyPad extends React.Component {
             { name: "col", val: props.col },
             { name: "table", val: props.table }
           ],
-          props.allowAnyEntry
+          props.allowAnyEntry,
+          props.currentActiveMoveName
+        )
+      : props.row !== state.row &&
+        props.col !== state.col &&
+        props.currentActiveMoveName !== state.currentActiveMoveName
+      ? state.buildDisableTable(
+          state.table,
+          props.row,
+          props.col,
+          [{ name: "row", val: props.row }, { name: "col", val: props.col }],
+          props.allowAnyEntry,
+          props.currentActiveMoveName
+        )
+      : props.row !== state.row &&
+        props.currentActiveMoveName !== state.currentActiveMoveName
+      ? state.buildDisableTable(
+          state.table,
+          props.row,
+          state.col,
+          [{ name: "row", val: props.row }],
+          props.allowAnyEntry,
+          props.currentActiveMoveName
+        )
+      : props.col !== state.col &&
+        props.currentActiveMoveName !== state.currentActiveMoveName
+      ? state.buildDisableTable(
+          state.table,
+          state.row,
+          props.col,
+          [{ name: "col", val: props.col }],
+          props.allowAnyEntry,
+          props.currentActiveMoveName
+        )
+      : props.table !== state.table &&
+        props.currentActiveMoveName !== state.currentActiveMoveName
+      ? state.buildDisableTable(
+          props.table,
+          state.row,
+          state.col,
+          [{ name: "table", val: props.table }],
+          props.allowAnyEntry,
+          props.currentActiveMoveName
+        )
+      : props.row !== state.row &&
+        props.col !== state.col &&
+        props.table !== state.table
+      ? state.buildDisableTable(
+          props.table,
+          props.row,
+          props.col,
+          [
+            { name: "row", val: props.row },
+            { name: "col", val: props.col },
+            { name: "table", val: props.table }
+          ],
+          props.allowAnyEntry,
+          state.currentActiveMoveName
         )
       : props.row !== state.row && props.col !== state.col
       ? state.buildDisableTable(
@@ -83,7 +199,8 @@ class SudokuKeyPad extends React.Component {
           props.row,
           props.col,
           [{ name: "row", val: props.row }, { name: "col", val: props.col }],
-          props.allowAnyEntry
+          props.allowAnyEntry,
+          state.currentActiveMoveName
         )
       : props.row !== state.row
       ? state.buildDisableTable(
@@ -91,7 +208,8 @@ class SudokuKeyPad extends React.Component {
           props.row,
           state.col,
           [{ name: "row", val: props.row }],
-          props.allowAnyEntry
+          props.allowAnyEntry,
+          state.currentActiveMoveName
         )
       : props.col !== state.col
       ? state.buildDisableTable(
@@ -99,7 +217,8 @@ class SudokuKeyPad extends React.Component {
           state.row,
           props.col,
           [{ name: "col", val: props.col }],
-          props.allowAnyEntry
+          props.allowAnyEntry,
+          state.currentActiveMoveName
         )
       : props.table !== state.table
       ? state.buildDisableTable(
@@ -107,7 +226,8 @@ class SudokuKeyPad extends React.Component {
           state.row,
           state.col,
           [{ name: "table", val: props.table }],
-          props.allowAnyEntry
+          props.allowAnyEntry,
+          state.currentActiveMoveName
         )
       : null;
   }
@@ -119,33 +239,36 @@ class SudokuKeyPad extends React.Component {
     var modal = document.getElementById("myModal");
     var dialog = document.getElementById("theKeyPad");
     for (let i = 0; i < this.state.watchForClicks.length; i++) {
-      document.getElementById(this.state.watchForClicks[i]).onclick = function(
-        e
+      document.getElementById(this.state.watchForClicks[i]).onclick = (function(
+        ctx
       ) {
-        e.target.classList.add("SudokuOptionsHighlighted");
-        modal.style.display = "block";
-        const newLeft = e.pageX + 20 - Math.floor(window.innerWidth / 2.0); /// e.pageX - 254;
-        const newTop =
-          e.pageY - 450 + Math.floor((window.innerHeight - 40) / 2.0); //e.pageY - 110;
-        dialog.style.left = "" + newLeft + "px";
-        dialog.style.top = "" + newTop + "px";
-        window.scrollTo(0, 65);
-      };
+        return function(e) {
+          const pos = ctx.findClickPosition(e);
+          e.target.classList.add("SudokuOptionsHighlighted");
+          modal.style.display = "block";
+          dialog.style.display = "block";
+          dialog.style.left = "" + pos.x - 400 + "px"; //e.clientX - 750
+          dialog.style.top = "" + pos.y - 575 + "px"; //pos.y - 650   //e.clientY - 650
+        };
+      })(this);
     }
     window.onresize = function() {
+      modal.style.display = "none";
+      dialog.style.display = "none";
       var elems = document.querySelectorAll(".SudokuOptionsHighlighted");
       [].forEach.call(elems, function(el) {
         el.classList.remove("SudokuOptionsHighlighted");
       });
-      modal.style.display = "none";
     };
     window.onclick = function(event) {
       if (event.target == modal) {
+        modal.style.display = "none";
+        dialog.style.display = "none";
         var elems = document.querySelectorAll(".SudokuOptionsHighlighted");
         [].forEach.call(elems, function(el) {
           el.classList.remove("SudokuOptionsHighlighted");
         });
-        modal.style.display = "none";
+        return true;
       }
     };
   }
@@ -158,6 +281,7 @@ class SudokuKeyPad extends React.Component {
   handleClick = numberClick => {
     this.resetHighlight();
     document.getElementById("myModal").style.display = "none";
+    document.getElementById("theKeyPad").style.display = "none";
     if (this.state.table[this.state.row][this.state.col] != numberClick) {
       this.state.setNumber(numberClick, this.state.row, this.state.col);
     }
@@ -165,110 +289,109 @@ class SudokuKeyPad extends React.Component {
   render() {
     return (
       <>
-        <div id="myModal" className="modal">
-          <div className="keypad-content" id="theKeyPad">
-            <table style={{ position: "relative", left: "-6px", top: "-5px" }}>
-              <tr>
-                <td>
-                  <button
-                    className="keyPadButton"
-                    onClick={e => this.handleClick(1, e)}
-                    disabled={this.state.disable1}
-                  >
-                    1
-                  </button>
-                </td>
-                <td>
-                  <button
-                    className="keyPadButton"
-                    onClick={e => this.handleClick(2, e)}
-                    disabled={this.state.disable2}
-                  >
-                    2
-                  </button>
-                </td>
-                <td>
-                  <button
-                    className="keyPadButton"
-                    onClick={e => this.handleClick(3, e)}
-                    disabled={this.state.disable3}
-                  >
-                    3
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <button
-                    className="keyPadButton"
-                    onClick={e => this.handleClick(4, e)}
-                    disabled={this.state.disable4}
-                  >
-                    4
-                  </button>
-                </td>
-                <td>
-                  <button
-                    className="keyPadButton"
-                    onClick={e => this.handleClick(5, e)}
-                    disabled={this.state.disable5}
-                  >
-                    5
-                  </button>
-                </td>
-                <td>
-                  <button
-                    className="keyPadButton"
-                    onClick={e => this.handleClick(6, e)}
-                    disabled={this.state.disable6}
-                  >
-                    6
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <button
-                    className="keyPadButton"
-                    onClick={e => this.handleClick(7, e)}
-                    disabled={this.state.disable7}
-                  >
-                    7
-                  </button>
-                </td>
-                <td>
-                  <button
-                    className="keyPadButton"
-                    onClick={e => this.handleClick(8, e)}
-                    disabled={this.state.disable8}
-                  >
-                    8
-                  </button>
-                </td>
-                <td>
-                  <button
-                    className="keyPadButton"
-                    onClick={e => this.handleClick(9, e)}
-                    disabled={this.state.disable9}
-                  >
-                    9
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td></td>
-                <td>
-                  <button
-                    className="keyPadButton"
-                    onClick={e => this.handleClick(".", e)}
-                  >
-                    {" - "}
-                  </button>
-                </td>
-                <td></td>
-              </tr>
-            </table>
-          </div>
+        <div id="myModal" className="modal"></div>
+        <div className="keypad-content" id="theKeyPad">
+          <table>
+            <tr>
+              <td>
+                <button
+                  className="keyPadButton"
+                  onClick={e => this.handleClick(1, e)}
+                  disabled={this.state.disable1}
+                >
+                  1
+                </button>
+              </td>
+              <td>
+                <button
+                  className="keyPadButton"
+                  onClick={e => this.handleClick(2, e)}
+                  disabled={this.state.disable2}
+                >
+                  2
+                </button>
+              </td>
+              <td>
+                <button
+                  className="keyPadButton"
+                  onClick={e => this.handleClick(3, e)}
+                  disabled={this.state.disable3}
+                >
+                  3
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <button
+                  className="keyPadButton"
+                  onClick={e => this.handleClick(4, e)}
+                  disabled={this.state.disable4}
+                >
+                  4
+                </button>
+              </td>
+              <td>
+                <button
+                  className="keyPadButton"
+                  onClick={e => this.handleClick(5, e)}
+                  disabled={this.state.disable5}
+                >
+                  5
+                </button>
+              </td>
+              <td>
+                <button
+                  className="keyPadButton"
+                  onClick={e => this.handleClick(6, e)}
+                  disabled={this.state.disable6}
+                >
+                  6
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <button
+                  className="keyPadButton"
+                  onClick={e => this.handleClick(7, e)}
+                  disabled={this.state.disable7}
+                >
+                  7
+                </button>
+              </td>
+              <td>
+                <button
+                  className="keyPadButton"
+                  onClick={e => this.handleClick(8, e)}
+                  disabled={this.state.disable8}
+                >
+                  8
+                </button>
+              </td>
+              <td>
+                <button
+                  className="keyPadButton"
+                  onClick={e => this.handleClick(9, e)}
+                  disabled={this.state.disable9}
+                >
+                  9
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan="3">
+                <button
+                  className="keyPadButton"
+                  onClick={e => this.handleClick(".", e)}
+                  style={{ width: "90px", height: "30px" }}
+                  disabled={this.state.disableclear}
+                >
+                  {" -clear- "}
+                </button>
+              </td>
+            </tr>
+          </table>
         </div>
       </>
     );
@@ -284,6 +407,9 @@ SudokuKeyPad.propTypes = {
   setDisable: PropTypes.func.isRequired,
   alphaCol: PropTypes.func.isRequired,
   currentActive: PropTypes.func.isRequired,
-  allowAnyEntry: PropTypes.bool.isRequired
+  allowAnyEntry: PropTypes.bool.isRequired,
+  sudokuTableName: PropTypes.string.isRequired,
+  //disableAll: PropTypes.func.isRequired
+  currentActiveMoveName: PropTypes.string.isRequired
 };
 export default SudokuKeyPad;
